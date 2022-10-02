bc4c9973645c888d8297e01d3e2e411715365b68
Anushka Purwar <anushkapurwar799@gmail.com>
2022-10-01 23:03:10 -0700

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++) arr[i]=sc.nextInt();
+        int search=sc.nextInt();
+        for(int i=0;i<size;i++){
+            if(arr[i]==search) System.out.println("Number found at index: "+i);
+        }
+    }
+}
